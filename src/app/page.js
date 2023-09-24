cccc8e9de71397da140cda839c7f7f738920ed64
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-24 20:13:24 +0500

    'root'

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,12 @@ export default function Home() {
     <div className='flex flex-row justify-center items-center w-full bg-green-600 hover:bg-green-800 lg:bg-rose-700 md:bg-amber-400 transition-all'>
       <Link href={'/blog'}>
         <h1 className='font-bold text-center py-4 text-3xl'>
-          <button className='btn bg-indigo-700 px-3 py-2 text-indigo-50 rounded-md mx-3'>Blogs</button>
+          <button className='btn bg-indigo-700 px-3 py-2 text-indigo-50 rounded-md mx-3 border-b-4 border-t-4'>Blogs</button>
         </h1>
       </Link>
       <Link href={'/image'}>
         <h1 className='font-bold text-center py-4 text-3xl'>
-          <button className='btn bg-indigo-700 px-3 py-2 text-indigo-50 rounded-md mx-3'>Image</button>
+          <button className='btn bg-indigo-700 px-3 py-2 text-indigo-50 rounded-md mx-3 border-b-4 border-t-4'>Image</button>
         </h1>
       </Link>
     </div>
