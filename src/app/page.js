eafcb6e60c1753abcef5b5d7044923176df40cd7
junaidsalam639 <junaidsalam639@gmail.com>
2023-09-25 12:37:43 +0500

    'api_fecth'

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-    <div className='flex flex-row justify-center items-center w-full bg-green-600 hover:bg-green-800 lg:bg-rose-700 md:bg-amber-400 transition-all py-5'>
+    <div className='flex flex-row justify-center items-center w-full py-5 bg-indigo-800'>
       <Link href={'/blog'}>
-          <button className='btn bg-indigo-700 px-4 py-2 text-indigo-50 rounded-md mx-3'>Blogs</button>
+          <button className='btn bg-indigo-700 px-4 py-2 text-indigo-50 rounded-md mx-3 hover:bg-indigo-600'>Blogs</button>
       </Link>
       <Link href={'/image'}>
-          <button className='btn bg-indigo-700 px-4 py-2 text-indigo-50 rounded-md mx-3'>Image</button>
+          <button className='btn bg-indigo-700 px-4 py-2 text-indigo-50 rounded-md mx-3 hover:bg-indigo-600'>Image</button>
       </Link>
       <Link href={'/product'}>
-          <button className='btn bg-indigo-700 px-4 py-2 text-indigo-50 rounded-md mx-3'>Product</button>
+          <button className='btn bg-indigo-700 px-4 py-2 text-indigo-50 rounded-md mx-3 hover:bg-indigo-600'>Product</button>
       </Link>
     </div>
     </>
